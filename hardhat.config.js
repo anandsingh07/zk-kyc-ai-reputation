33edef861279f45ec5e4920ae866ad2b629a7303
Anand Singh <anandsinghas9777@gmail.com>
2025-07-01 21:40:42 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     sepolia: {
       url: "https://eth-sepolia.g.alchemy.com/v2/NPDIFQZwAYGEs62pu65Wq",
-      accounts: ["bac02fb51a3c68c80092dbae2d25463e3471368877e9ebc68e2de838f165e88c"],
+      accounts: [""],
     },
   },
 };
